51cef0ce5c10be5f6d7b58e039d356198a2170e8
Karthik Pillalamarri <karthik.pillalamarri.kp@gmail.com>
2024-07-20 21:13:24 +0000

    created shouty server and tested by sending udp packets to localhost ip address and port that udp socket of shouty server program is bound to

--- Sockets/sockets.py
+++ Sockets/sockets.py
@@ -1,5 +1,9 @@
 #Using file interface in Python which wraps an underlying interface
 import socket 
 
+HOST = '0.0.0.0'
+PORT = 8080
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-udp_socket.bind('0.0.0.0')
+udp_socket.bind((HOST, PORT))
+message, addr = udp_socket.recvfrom(1024)
+udp_socket.sendto(message.upper(), addr)
