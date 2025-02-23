ea660f1827d41a78de58d3c49667690100c246bc
Karthik Pillalamarri <karthik.pillalamarri.kp@gmail.com>
2025-02-23 22:33:11 +0000

    wrote some code to implement proxy, etc.

---
+++ HTTP_Proxy/proxy-basic/proxy.py
@@ -0,0 +1,13 @@
+#Specification:  Write a program which accepts a single incoming TCP connection, 
+#forwards a single message to an upstream server (such as python3 -m http.server), 
+#listens for a response, then forwards back the response to the client, before closing the connection
+
+def forward_message_to_server(dest_host, dest_port):
+    pass
+
+def main():
+    proxy_host = '0.0.0.0'
+    proxy_port = 8000
+    server_host = 'localhost'
+    server_port = 9000
+    pass
